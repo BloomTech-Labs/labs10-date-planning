4ffe2e32588fa9db33093a66f14d5b9da63e61eb
Lauren <llwor94@gmail.com>
2019-02-13 17:46:47 -0800

    removed dem modules

--- frontend/node_modules/dayjs/esm/plugin/isBetween/index.js
+++
@@ -1,7 +0,0 @@
-export default (function (o, c, d) {
-  c.prototype.isBetween = function (a, b, u) {
-    var dA = d(a);
-    var dB = d(b);
-    return this.isAfter(dA, u) && this.isBefore(dB, u) || this.isBefore(dA, u) && this.isAfter(dB, u);
-  };
-});
