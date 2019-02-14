f6b8d133aa0d4f2f64c5cf06429f5107afe3f089
Trevor Fehrman <trevorfehrman@gmail.com>
2019-02-14 02:31:30 -0800

    generated terms and conditions and added component on splash

--- frontend/components/SplashPage/Register.js
+++ frontend/components/SplashPage/Register.js
@@ -12,7 +12,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import MusicNote from "@material-ui/icons/MusicNote";
 import TheaterMasks from "../../static/img/icons/TheaterMasks";
 import Restaurant from "@material-ui/icons/Restaurant";
-import Group from "@material-ui/icons/Group";
 import Face from "@material-ui/icons/Face";
 import Icon from "@material-ui/core/Icon";
 import Email from "@material-ui/icons/Email";
@@ -28,8 +27,11 @@ import InfoArea from "../../styledComponents/InfoArea/InfoArea";
 
 import Styles from "../../static/jss/material-kit-pro-react/views/componentsSections/javascriptStyles";
 
+import Terms from "../../components/SplashPage/Terms";
+
 const Register = ({ classes }) => {
   const [modalShowing, setModalShowing] = useState(false);
+  const [termsShowing, setTermsShowing] = useState(false);
 
   return (
     <Fragment>
@@ -44,172 +46,188 @@ const Register = ({ classes }) => {
         open={modalShowing}
         // TransitionComponent={Transition}
         keepMounted
-        onClose={() => setModalShowing(false)}
+        onClose={() => {setModalShowing(false)}}
         aria-labelledby="signup-modal-slide-title"
         aria-describedby="signup-modal-slide-description"
       >
-        <Card plain className={classes.modalSignupCard}>
-          <DialogTitle
-            id="signup-modal-slide-title"
-            disableTypography
-            className={classes.modalHeader}
-          >
-            <Button
-              simple
-              className={classes.modalCloseButton}
-              key="close"
-              aria-label="Close"
-              onClick={() => setModalShowing(false)}
-            >
-              {" "}
-              <Close className={classes.modalClose} />
-            </Button>
-            <h3 className={`${classes.cardTitle} ${classes.modalTitle}`}>
-              Register
-            </h3>
-          </DialogTitle>
-          <DialogContent
-            id="signup-modal-slide-description"
-            className={classes.modalBody}
-          >
-            <GridContainer>
-              <GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
-                <InfoArea
-                  className={classes.infoArea}
-                  title="Concerts"
-                  description={
-                    <p>
-                      Find shows near you according to your tastes and get
-                      notified when your favorite performers are in town.
-                    </p>
-                  }
-                  icon={MusicNote}
-                  iconColor="rose"
-                />
-                <InfoArea
-                  className={classes.infoArea}
-                  title="Comedy and Theater"
-                  description={
-                    <p>
-                      Get the scoop on nearby comedy and theatrical events as well as other kind of live performances.
-                    </p>
-                  }
-                  icon={TheaterMasks}
-                  iconColor="primary"
-                />
-                <InfoArea
-                  className={classes.infoArea}
-                  title="Epicurean Adventures"
-                  description={
-                    <p>
-                      Be the first to make a reservation at nearby restaurant grand openings or prix fixe events.
-                    </p>
-                  }
-                  icon={Restaurant}
-                  iconColor="info"
-                />
-              </GridItem>
-              <GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
-                <div className={classes.textCenter}>
-                  <Button justIcon round color="google">
-                    <i className="fab fa-google" />
+        {
+          <Card plain className={classes.modalSignupCard}>
+            {termsShowing ? (
+              <Terms setTermsShowing={setTermsShowing}/>
+            ) : (
+              <div>
+                <DialogTitle
+                  id="signup-modal-slide-title"
+                  disableTypography
+                  className={classes.modalHeader}
+                >
+                  <Button
+                    simple
+                    className={classes.modalCloseButton}
+                    key="close"
+                    aria-label="Close"
+                    onClick={() => setModalShowing(false)}
+                  >
+                    {" "}
+                    <Close className={classes.modalClose} />
                   </Button>
-
-                  <Button justIcon round color="facebook">
-                    <i className="fab fa-facebook-f" />
-                  </Button>
-                  <Button justIcon round color="instagram">
-                    <i className="fab fa-instagram" />
-                  </Button>
-
-                  <h4 className={classes.socialTitle}>or be classical</h4>
-                </div>
-                <form className={classes.form}>
-                  <CustomInput
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.customFormControlClasses
-                    }}
-                    inputProps={{
-                      startAdornment: (
-                        <InputAdornment
-                          position="start"
-                          className={classes.inputAdornment}
-                        >
-                          <Face className={classes.inputAdornmentIcon} />
-                        </InputAdornment>
-                      ),
-                      placeholder: "Full Name..."
-                    }}
-                  />
-                  <CustomInput
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.customFormControlClasses
-                    }}
-                    inputProps={{
-                      startAdornment: (
-                        <InputAdornment
-                          position="start"
-                          className={classes.inputAdornment}
-                        >
-                          <Email className={classes.inputAdornmentIcon} />
-                        </InputAdornment>
-                      ),
-                      placeholder: "Email..."
-                    }}
-                  />
-                  <CustomInput
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.customFormControlClasses
-                    }}
-                    inputProps={{
-                      startAdornment: (
-                        <InputAdornment
-                          position="start"
-                          className={classes.inputAdornment}
-                        >
-                          <Icon className={classes.inputAdornmentIcon}>
-                            lock_outline
-                          </Icon>
-                        </InputAdornment>
-                      ),
-                      placeholder: "Password..."
-                    }}
-                  />
-                  <FormControlLabel
-                    classes={{
-                      label: classes.label
-                    }}
-                    control={
-                      <Checkbox
-                        tabIndex={-1}
-                        // onClick={() => handleToggle(1)}
-                        checkedIcon={<Check className={classes.checkedIcon} />}
-                        icon={<Check className={classes.uncheckedIcon} />}
-                        classes={{
-                          checked: classes.checked,
-                          root: classes.checkRoot
-                        }}
+                  <h3 className={`${classes.cardTitle} ${classes.modalTitle}`}>
+                    Register
+                  </h3>
+                </DialogTitle>
+                <DialogContent
+                  id="signup-modal-slide-description"
+                  className={classes.modalBody}
+                >
+                  <GridContainer>
+                    <GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
+                      <InfoArea
+                        className={classes.infoArea}
+                        title="Concerts"
+                        description={
+                          <p>
+                            Find shows near you according to your tastes and get
+                            notified when your favorite performers are in town.
+                          </p>
+                        }
+                        icon={MusicNote}
+                        iconColor="rose"
                       />
-                    }
-                    label={
-                      <span>
-                        I agree to the <a href="#">terms and conditions</a>.
-                      </span>
-                    }
-                  />
-                  <div className={classes.textCenter}>
-                    <Button round color="primary">
-                      Get started
-                    </Button>
-                  </div>
-                </form>
-              </GridItem>
-            </GridContainer>
-          </DialogContent>
-        </Card>
+                      <InfoArea
+                        className={classes.infoArea}
+                        title="Comedy and Theater"
+                        description={
+                          <p>
+                            Get the scoop on nearby comedy and theatrical events
+                            as well as other kind of live performances.
+                          </p>
+                        }
+                        icon={TheaterMasks}
+                        iconColor="primary"
+                      />
+                      <InfoArea
+                        className={classes.infoArea}
+                        title="Epicurean Adventures"
+                        description={
+                          <p>
+                            Be the first to make a reservation at nearby
+                            restaurant grand openings or prix fixe events.
+                          </p>
+                        }
+                        icon={Restaurant}
+                        iconColor="info"
+                      />
+                    </GridItem>
+                    <GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
+                      <div className={classes.textCenter}>
+                        <Button justIcon round color="google">
+                          <i className="fab fa-google" />
+                        </Button>
+
+                        <Button justIcon round color="facebook">
+                          <i className="fab fa-facebook-f" />
+                        </Button>
+                        <Button justIcon round color="instagram">
+                          <i className="fab fa-instagram" />
+                        </Button>
+
+                        <h4 className={classes.socialTitle}>or be classical</h4>
+                      </div>
+                      <form className={classes.form}>
+                        <CustomInput
+                          formControlProps={{
+                            fullWidth: true,
+                            className: classes.customFormControlClasses
+                          }}
+                          inputProps={{
+                            startAdornment: (
+                              <InputAdornment
+                                position="start"
+                                className={classes.inputAdornment}
+                              >
+                                <Face className={classes.inputAdornmentIcon} />
+                              </InputAdornment>
+                            ),
+                            placeholder: "Full Name..."
+                          }}
+                        />
+                        <CustomInput
+                          formControlProps={{
+                            fullWidth: true,
+                            className: classes.customFormControlClasses
+                          }}
+                          inputProps={{
+                            startAdornment: (
+                              <InputAdornment
+                                position="start"
+                                className={classes.inputAdornment}
+                              >
+                                <Email className={classes.inputAdornmentIcon} />
+                              </InputAdornment>
+                            ),
+                            placeholder: "Email..."
+                          }}
+                        />
+                        <CustomInput
+                          formControlProps={{
+                            fullWidth: true,
+                            className: classes.customFormControlClasses
+                          }}
+                          inputProps={{
+                            startAdornment: (
+                              <InputAdornment
+                                position="start"
+                                className={classes.inputAdornment}
+                              >
+                                <Icon className={classes.inputAdornmentIcon}>
+                                  lock_outline
+                                </Icon>
+                              </InputAdornment>
+                            ),
+                            placeholder: "Password..."
+                          }}
+                        />
+                        <FormControlLabel
+                          classes={{
+                            label: classes.label
+                          }}
+                          control={
+                            <Checkbox
+                              tabIndex={-1}
+                              // onClick={() => handleToggle(1)}
+                              checkedIcon={
+                                <Check className={classes.checkedIcon} />
+                              }
+                              icon={<Check className={classes.uncheckedIcon} />}
+                              classes={{
+                                checked: classes.checked,
+                                root: classes.checkRoot
+                              }}
+                            />
+                          }
+                          label={
+                            <span>
+                              I agree to the{" "}
+                              <a onClick={()=>setTermsShowing(true)} href="#">
+                                terms and conditions
+                              </a>
+                              .
+                            </span>
+                          }
+                        />
+                        <div className={classes.textCenter}>
+                          <Button round color="primary">
+                            Get started
+                          </Button>
+                        </div>
+                      </form>
+                    </GridItem>
+                  </GridContainer>
+                </DialogContent>
+              </div>
+            )}
+          </Card>
+        }
       </Dialog>
     </Fragment>
   );
