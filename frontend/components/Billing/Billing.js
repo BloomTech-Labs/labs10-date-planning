a9a819a80c40cbea5a60cfaacfa224ca1c5e1ffb
ykrueng <yasirah.krueng@gmail.com>
2019-02-15 13:18:06 -0600

    add basic styling to billing page

--- frontend/components/Billing/Billing.js
+++ frontend/components/Billing/Billing.js
@@ -1,29 +1,148 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import User from '../Queries/User';
-import Stripe from './Stripe';
+import User from "../Queries/User";
+import Stripe from "./Stripe";
 
-const Billing = () => {
-	return (
-		<User>
-			{({ data: { currentUser } }) => {
-				const currentSubs = currentUser.permissions[0];
-				return (
-					<div>
-						<h1>Billing</h1>
-						<Stripe subsType="MONTHLY">
-							<button disabled={currentSubs === 'MONTHLY' || currentSubs === 'YEARLY'}>
-								Monthly
-							</button>
-						</Stripe>
-						<Stripe subsType="YEARLY">
-							<button disabled={currentSubs === 'YEARLY'}>Premium</button>
-						</Stripe>
-					</div>
-				);
-			}}
-		</User>
-	);
+import CardHeader from "../../styledComponents/Card/CardHeader";
+
+import classNames from "classnames";
+// core components
+import GridContainer from "../../styledComponents/Grid/GridContainer.jsx";
+import GridItem from "../../styledComponents/Grid/GridItem.jsx";
+import Card from "../../styledComponents/Card/Card.jsx";
+import CardBody from "../../styledComponents/Card/CardBody.jsx";
+import Button from "../../styledComponents/CustomButtons/Button.jsx";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import pricingStyle from "../../static/jss/material-kit-pro-react/views/pricingSections/pricingStyle.jsx";
+
+const Billing = (props) => {
+  const { classes } = props;
+
+  return (
+    <User>
+      {({ data: { currentUser } }) => {
+        const currentSubs = currentUser.permissions[0];
+        return (
+          <div
+            className={classes.pricingSection}
+          >
+            <GridContainer
+              style={{
+                justifyItems: 'center'
+              }}
+            >
+              <GridItem
+                md={6}
+                sm={6}
+                lg={6}
+                className={classNames(
+                  classes.mrAuto,
+                  classes.mlAuto,
+                  classes.textCenter
+                )}
+              >
+                <h2>Pricing</h2>
+              </GridItem>
+            </GridContainer>
+            <GridContainer>
+              <GridItem md={4} sm={4}>
+                <Card pricing plain>
+                  <CardBody pricing>
+                  <h6
+                      className={classNames(classes.cardCategory, classes.textInfo)}
+                    >
+                      Montly Basics
+                    </h6>
+                    <h1 className={classes.cardTitle}>
+                      <small>$</small>9.99 <small>/month</small>
+                    </h1>
+                    <ul>
+                      <li>
+                        <b>500</b> Project
+                      </li>
+                      <li>
+                        <b>50</b> Team Members
+                      </li>
+                      <li>
+                        <b>125</b> Personal Contacts
+                      </li>
+                      <li>
+                        <b>15.000</b> Messages
+                      </li>
+                    </ul>
+                    {
+                      currentSubs === 'FREE' &&
+                      <Stripe subsType="MONTHLY" user={currentUser}>
+                        <Button
+                          color='primary'
+                          round>
+                          CHECKOUT
+                        </Button>
+                      </Stripe>
+                    }
+                    {
+                      currentSubs === 'MONTHLY' &&
+                      <Button
+                        color='rose'
+                        disabled
+                        round>
+                        CURRENT SUBSCRIPTION
+                      </Button>
+                    }
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem
+                md={6}
+                sm={6}
+                lg={6}
+              >
+                <Card plain pricing>
+                  <CardBody pricing>
+                    <h6
+                      className={classNames(classes.cardCategory, classes.textInfo)}
+                    >
+                      Yearly Premium
+                    </h6>
+                    <h1 className={classes.cardTitle}>
+                      <small>$</small>29.99 <small>/year</small>
+                    </h1>
+                    <ul>
+                      <li>
+                        <b>1000</b> Project
+                      </li>
+                      <li>
+                        <b>100</b> Team Members
+                      </li>
+                      <li>
+                        <b>550</b> Personal Contacts
+                      </li>
+                      <li>
+                        <b>50.000</b> Messages
+                      </li>
+                    </ul>
+                    {
+                      currentSubs !== 'YEARLY' &&
+                      <Stripe subsType="YEARLY" user={currentUser}>
+                        <Button
+                          color='primary'
+                          round
+                        >
+                        CHECKOUT
+                        </Button>
+                      </Stripe>
+                    }
+                  </CardBody>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+        );
+      }}
+    </User>
+  );
 };
 
-export default Billing;
+export default withStyles(pricingStyle)(Billing);
--- frontend/components/Billing/Stripe.js
+++ frontend/components/Billing/Stripe.js
@@ -6,49 +6,53 @@ import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 
 const CREATE_ORDER_MUTATION = gql`
-	mutation createOrder($token: String!, $subscription: Permission!) {
-		createOrder(token: $token, subscription: $subscription) {
-			id
-			total
-		}
-	}
+  mutation createOrder($token: String!, $subscription: Permission!) {
+    createOrder(token: $token, subscription: $subscription) {
+      id
+      total
+    }
+  }
 `;
 
 const Billing = props => {
-	const onToken = async (res, subsType, createOrder) => {
-		await createOrder({
-			variables: {
-				token: res.id,
-				subscription: subsType
-			}
-		}).catch(err => {
-			alert(err.message);
-		});
-		Router.push({
-			pathname: '/'
-		});
-		return null;
-	};
+  const onToken = async (res, subsType, createOrder) => {
+    await createOrder({
+      variables: {
+        token: res.id,
+        subscription: subsType
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
+    Router.push({
+      pathname: '/'
+    });
+    return null
+  }
 
-	return (
-		<Mutation mutation={CREATE_ORDER_MUTATION}>
-			{createOrder => {
-				return (
-					<StripeCheckout
-						amount={props.subsType === 'MONTHLY' ? 999 : 2999}
-						name="Up4"
-						description="One year subscription"
-						stripeKey="pk_test_XMzhj8sz1Y1twtwn6sLLpy9C"
-						currency="USD"
-						email={'boom@boom.com'}
-						token={res => onToken(res, props.subsType, createOrder)}
-					>
-						<div>{props.children}</div>
-					</StripeCheckout>
-				);
-			}}
-		</Mutation>
-	);
+  return (
+    <Mutation mutation={CREATE_ORDER_MUTATION}>
+      {
+        (createOrder) => {
+          return (
+            <StripeCheckout
+              amount={props.subsType === 'MONTHLY' ? 999 : 2999}
+              name="Up4"
+              description="One year subscription"
+              stripeKey="pk_test_XMzhj8sz1Y1twtwn6sLLpy9C"
+              currency="USD"
+              email={props.user.email}
+              token={res => onToken(res, props.subsType, createOrder)}
+            >
+              <div>
+                {props.children}
+              </div>
+            </StripeCheckout>
+          );
+        }
+      }
+    </Mutation>
+  );
 };
 
 export default Billing;
