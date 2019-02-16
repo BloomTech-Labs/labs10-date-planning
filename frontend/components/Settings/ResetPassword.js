ee233e4101730bccb9bd717897af2e1370bbf999
Holden B <holden.bucher.92@gmail.com>
2019-02-15 19:32:07 -0600

    awful password reset at least mocked out

--- frontend/components/Settings/ResetPassword.js
+++ frontend/components/Settings/ResetPassword.js
@@ -1,42 +1,128 @@
-// import React from 'react';
-import React from 'react';
-import classNames from 'classnames';
-// import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
+import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
+import withStyles from '@material-ui/core/styles/withStyles';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-// import MenuItem from '@material-ui/core/MenuItem';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Icon from '@material-ui/core/Icon';
+import gql from 'graphql-tag';
+import Styles from '../../static/jss/material-kit-pro-react/views/componentsSections/javascriptStyles';
+import CustomInput from '../../styledComponents/CustomInput/CustomInput';
+import GridContainer from '../../styledComponents/Grid/GridContainer';
+import GridItem from '../../styledComponents/Grid/GridItem';
+import Button from '../../styledComponents/CustomButtons/Button';
+import { CURRENT_USER_QUERY } from '../Queries/User';
 
-const ResetPassword = () => {
+const SIMPLE_PASS_RESET = gql`
+	mutation SIMPLE_PASS_RESET(
+		$oldPassword: String!
+		$newPassword1: String!
+		$newPassword2: String!
+	) {
+		internalPasswordReset(
+			oldPassword: $oldPassword
+			newPassword1: $newPassword1
+			newPassword2: $newPassword2
+		) {
+			id
+			firstName
+			lastName
+			email
+		}
+	}
+`;
+
+const ResetPassword = props => {
+	const { classes } = props;
+	const [password, setPassword] = useState({ oldPassword: '', newPassword1: '', newPassword2: '' });
 	return (
-		<div>
-			<p>Im here to reset your password bitccccc</p>
-			<TextField
-				id="outlined-adornment-password"
-				className={classNames(classes.margin, classes.textField)}
-				variant="outlined"
-				type={this.state.showPassword ? 'text' : 'password'}
-				label="Password"
-				value={this.state.password}
-				onChange={this.handleChange('password')}
-				InputProps={{
-					endAdornment: (
-						<InputAdornment position="end">
-							<IconButton
-								aria-label="Toggle password visibility"
-								onClick={this.handleClickShowPassword}
-							>
-								{this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-							</IconButton>
-						</InputAdornment>
-					)
-				}}
-			/>
-		</div>
+		<Mutation
+			mutation={SIMPLE_PASS_RESET}
+			variables={{ ...password }}
+			refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+		>
+			{(internalPasswordReset, { loading, error }) => (
+				<GridContainer justify="column">
+					<GridItem xs={6} style={{ margin: '0 auto' }}>
+						<CustomInput
+							labelText="Old Password"
+							type="password"
+							name="oldpassword"
+							required
+							id="material"
+							formControlProps={{
+								fullWidth: true
+							}}
+							inputProps={{
+								placeholder: 'Current password...',
+								value: password.oldPassword,
+								onChange: ({ target: { name, value } }) =>
+									setPassword({
+										...password,
+										[name]: value
+									})
+							}}
+						/>
+					</GridItem>
+					<GridItem xs={6} style={{ margin: '0 auto' }}>
+						<CustomInput
+							labelText="New Password"
+							type="password"
+							name="newPassword1"
+							required
+							id="material"
+							formControlProps={{
+								fullWidth: true
+							}}
+							inputProps={{
+								placeholder: 'New password...',
+								value: password.newPassword1,
+								onChange: ({ target: { name, value } }) =>
+									setPassword({
+										...password,
+										[name]: value
+									})
+							}}
+						/>
+					</GridItem>
+					<GridItem xs={8} style={{ margin: '0 auto' }}>
+						<CustomInput
+							labelText="Confirm Password"
+							type="password"
+							name="newPassword2"
+							required
+							id="material"
+							formControlProps={{
+								fullWidth: true
+							}}
+							inputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<Icon className={classes.icon}>lock_outline</Icon>
+									</InputAdornment>
+								),
+								placeholder: 'Confirm Password...',
+								value: password.newPassword2,
+								onChange: ({ target: { name, value } }) =>
+									setPassword({
+										...password,
+										[name]: value
+									})
+							}}
+						/>
+						<Button
+							color="primary"
+							size="lg"
+							disabled={loading || error}
+							onClick={() => {
+								internalPasswordReset();
+							}}
+						>
+							Confirm Reset
+						</Button>
+					</GridItem>
+				</GridContainer>
+			)}
+		</Mutation>
 	);
 };
 
-export default ResetPassword;
+export default withStyles(Styles)(ResetPassword);
