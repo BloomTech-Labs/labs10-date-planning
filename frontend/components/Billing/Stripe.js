da919d0cf8170c6e5450b0edb9b6ee5a0b75011b
ykrueng <yasirah.krueng@gmail.com>
2019-02-17 00:38:09 -0600

    add refetchQueries to create order mutation

--- frontend/components/Billing/Stripe.js
+++ frontend/components/Billing/Stripe.js
@@ -5,6 +5,10 @@ import Router from 'next/router';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 
+import { CURRENT_USER_QUERY } from '../Queries/User';
+import { GET_REMAINING_DATES } from './DatesLeft';
+import { GET_USER_ORDER } from './TransactionList'
+
 const CREATE_ORDER_MUTATION = gql`
   mutation createOrder($token: String!, $subscription: Permission!) {
     createOrder(token: $token, subscription: $subscription) {
@@ -31,7 +35,14 @@ const Billing = props => {
   }
 
   return (
-    <Mutation mutation={CREATE_ORDER_MUTATION}>
+    <Mutation
+      mutation={CREATE_ORDER_MUTATION}
+      refetchQueries={[
+        { query: CURRENT_USER_QUERY },
+        { query: GET_REMAINING_DATES },
+        { query: GET_USER_ORDER, variables: {userId: props.user.id} }
+      ]}
+    >
       {
         (createOrder) => {
           return (
