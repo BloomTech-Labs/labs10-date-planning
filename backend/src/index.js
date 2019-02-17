bc39d8d0fa5377c9fb5945e4db0339d1c07a0d5b
Holden B <holden.bucher.92@gmail.com>
2019-02-17 17:13:46 -0600

    hooked up firebase for google auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,38 +2,26 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: './.env' });
+const { verifyUserToken } = require('./firebase/firebase');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
-
 server.express.use(cookieParser());
 
 // commented out bc it messes up testing queries and mutations in the graphql playground
-server.express.use((req, res, next) => {
-	const { token } = req.cookies;
+server.express.use(async (req, res, next) => {
+	const { token, session } = req.cookies;
 	if (token) {
 		const { userId } = jwt.verify(token, process.env.APP_SECRET);
 		req.userId = userId;
 	}
+	if (session) {
+		const firebaseUser = await verifyUserToken(session);
+		req.userId = req.cookies.userId;
+	}
 	next();
 });
-
-
-
-// maybe a decent way to setup cors for app since we have multiple URLs
-// server.use(function(req, res, next) {
-// 	var allowedOrigins = ['up4api.heroku.com', 'http://localhost:3000'];
-// 	var origin = req.headers.origin;
-// 	if (allowedOrigins.indexOf(origin) > -1) {
-// 		res.setHeader('Access-Control-Allow-Origin', origin);
-// 	}
-// 	//res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-// 	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 	res.header('Access-Control-Allow-Credentials', true);
-// 	return next();
-// });
 
 server.start(
 	{
