709f7c7d660f1fdeec7044aef4b665a72e2e91db
Holden B <holden.bucher.92@gmail.com>
2019-02-17 17:15:16 -0600

    added firebase things

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,10 @@
 import Splash from '../components/SplashPage';
-import User from '../components/Queries/User'
-import Home from './home'
+import User from '../components/Queries/User';
+import Home from './home';
 
 const Index = () => (
-  <User>
-    {({data: {currentUser}}) => (
-      <>
-      {currentUser ? <Home /> : <Splash />}
-      </>
-    )}
-  </User>
-
+	<User>{({ data: { currentUser } }) => <>{currentUser ? <Home /> : <Splash />}</>}</User>
 );
 
 export default Index;
+//
