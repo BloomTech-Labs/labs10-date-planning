d0ebc08cf38f8fcb3c173b3dcaa929512a0e7838
Holden B <holden.bucher.92@gmail.com>
2019-02-17 21:06:34 -0600

    troubleshooting env issues

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,18 +1,14 @@
 import Splash from '../components/SplashPage';
-import User from '../components/Queries/User'
-import Home from './home'
+import User from '../components/Queries/User';
+import Home from './home';
 
 const Index = () => (
-  <User>
-    {({data: {currentUser}}) => {
-      console.log(currentUser)
-      return (
-      <>
-      {currentUser ? <Home /> : <Splash />}
-      </>
-    )}}
-  </User>
-
+	<User>
+		{({ data: { currentUser } }) => {
+			console.log(currentUser);
+			return <>{currentUser ? <Home /> : <Splash />}</>;
+		}}
+	</User>
 );
 
 export default Index;
