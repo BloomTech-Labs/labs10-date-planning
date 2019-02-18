1a76b9bf80f42847d46cf688c6cd124b7156681e
Holden B <holden.bucher.92@gmail.com>
2019-02-17 19:14:18 -0600

    fixed firebase

--- backend/src/firebase/firebase.js
+++ backend/src/firebase/firebase.js
@@ -1,9 +1,15 @@
 const firebaseAdmin = require('firebase-admin');
-const serviceAccount = require('./creds');
 
 const admin = firebaseAdmin.initializeApp(
 	{
-		credential: firebaseAdmin.credential.cert(serviceAccount),
+		credential: firebaseAdmin.credential.cert({
+			type: 'service_account',
+			project_id: process.env.FIREBASE_PROJECT_ID,
+			private_key_id: process.PRIVATE_KEY_ID,
+			private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+			client_email: process.env.FIREBASE_CLIENT_EMAIL,
+			client_id: process.env.FIREBASE_CLIENT_ID
+		}),
 		databaseURL: process.env.FIREBASE_DATABASE_URL
 	},
 	'server'
