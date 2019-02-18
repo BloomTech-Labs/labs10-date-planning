86cd833930d5ffe073194e2a9f62660d24eb2827
Holden B <holden.bucher.92@gmail.com>
2019-02-17 18:11:37 -0600

    removed secret

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-	apiKey: 'AIzaSyDQ1wUgadFuPSpRppfG9bmawP8H6HMgfQU',
+	apiKey: process.env.API_KEY,
 	authDomain: 'up4-life.firebaseapp.com',
 	databaseURL: 'https://up4-life.firebaseio.com',
 	projectId: 'up4-life',
