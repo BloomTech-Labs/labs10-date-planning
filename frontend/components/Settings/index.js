a1801bc0a6a230e34141c3c7d8414d7c0037130f
Trevor Fehrman <trevorfehrman@gmail.com>
2019-02-20 11:19:25 -0800

    stuff

--- frontend/components/Settings/index.js
+++ frontend/components/Settings/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import GridContainer from '../../styledComponents/Grid/GridContainer';
-import GridItem from '../../styledComponents/Grid/GridItem';
-import Dates from './Dates';
-import withStyles from '@material-ui/core/styles/withStyles';
-import blogsStyle from '../../static/jss/material-kit-pro-react/views/sectionsSections/blogsStyle.jsx';
+import React from "react";
+import Sidebar from "./Sidebar";
+import GridContainer from "../../styledComponents/Grid/GridContainer";
+import GridItem from "../../styledComponents/Grid/GridItem";
+import Dates from "./Dates";
+import withStyles from "@material-ui/core/styles/withStyles";
+import blogsStyle from "../../static/jss/material-kit-pro-react/views/sectionsSections/blogsStyle.jsx";
+import Drawer from "@material-ui/core/Drawer";
+
+import "../../styles/Settings/index.scss";
 
 const Settings = ({ classes }) => {
-	return (
-		<div style={{ height: '100%' }}>
-			<GridContainer >
-				<GridItem container justify='center' md={4} lg={4}>
-					<Sidebar />
-				</GridItem>
-				<GridItem md={8} lg={8}>
-					<Dates />
-				</GridItem>
-			</GridContainer>
-		</div>
-	);
+  return (
+    <div>
+      <Drawer anchor="left" variant="persistent" open={false} className="drawer">
+        <Sidebar />
+      </Drawer>
+
+      <Dates />
+    </div>
+  );
 };
 
 export default withStyles(blogsStyle)(Settings);
