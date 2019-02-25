0a8ce68999757c5f716e64426d4130d79f7c7057
Lauren <llwor94@gmail.com>
2019-02-25 01:13:53 -0800

    fixed billing thing

--- frontend/pages/billing.js
+++ frontend/pages/billing.js
@@ -4,15 +4,21 @@ import User from '../components/Queries/User';
 import JoinUs from './joinus';
 
 const BillingPage = () => (
-	
+	<User>
+		{({ loading, error, data: { currentUser } }) => {
+			if (loading) return <div>Loading...</div>
+			if (error || !currentUser) return <JoinUs />
 		
-		
+			return (
 				<>
 					<Billing currentUser={currentUser} />
 					<TransactionList currentUser={currentUser} />
 				</>
+			);
+		}}
+			</User>
 			
-}
+)
 
 BillingPage.getInitialProps = async ctx => {
 	let user = await isLoggedIn(ctx.apolloClient);
