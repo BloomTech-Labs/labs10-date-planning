649be1072507a074fdbbdce57f2efe17263afd72
ykrueng <yasirah.krueng@gmail.com>
2019-02-24 18:17:00 -0600

    refactor permission

--- frontend/components/Settings/Dates.js
+++ frontend/components/Settings/Dates.js
@@ -30,7 +30,7 @@ const Dates = ({ classes }) => {
 								</div>
 							)}
 						</GridContainer>
-						{currentUser.permissions[0] === 'FREE' && (
+						{currentUser.permissions === 'FREE' && (
 							<div className='Snackbar'>
 								<SnackbarContent
 									message={
