5ee8587d14af01b871338b136f0018cb99f450b8
Lauren <llwor94@gmail.com>
2019-02-25 23:09:27 -0800

    displaying user age and set width on drawer

--- frontend/components/Home/Event.js
+++ frontend/components/Home/Event.js
@@ -22,10 +22,10 @@ import Button from '../../styledComponents/CustomButtons/Button';
 import CardHeader from '../../styledComponents/Card/CardHeader';
 import CardFooter from '../../styledComponents/Card/CardFooter';
 import CardBody from '../../styledComponents/Card/CardBody';
-
+//utils
+import getAge from '../../utils/getAge';
 //styles
 import CardStyles from '../../static/jss/material-kit-pro-react/views/componentsSections/sectionCards';
-
 import '../../styles/Home/Event.scss';
 
 function useForceUpdate() {
@@ -252,6 +252,8 @@ const Event = ({ event, classes, user, location }) => {
 									<TableRow>
 										<TableCell>{''}</TableCell>
 										<TableCell>{''}</TableCell>
+										<TableCell>{''}</TableCell>
+										<TableCell>{''}</TableCell>
 									</TableRow>
 								</TableHead>
 								<TableBody>
@@ -268,6 +270,8 @@ const Event = ({ event, classes, user, location }) => {
 												/>
 											</TableCell>
 											<TableCell>{user.firstName}</TableCell>
+											<TableCell>{getAge(user.dob)}</TableCell>
+											<TableCell>{user.gender.toLowerCase()}</TableCell>
 										</TableRow>
 									))}
 								</TableBody>
--- frontend/components/Profile/index.js
+++ frontend/components/Profile/index.js
@@ -1,31 +1,37 @@
 import React, { useState } from 'react';
 import User from '../Queries/User';
 import Location from '../Settings/Location';
+import getAge from '../../utils/getAge';
 
 import '../../styles/Profile/index.scss';
 const Profile = () => {
 	return (
 		<User>
-			{({ data: { currentUser }, client }) => (
-				<div className='Profile-Header'>
-					<div className='inner'>
-						<div
-							className='prof-img'
-							style={{ backgroundImage: `url(${currentUser.imageLarge})` }}
-						/>
-						<div
-							style={{
-								display: 'flex',
-								flexDirection: 'column',
-								paddingLeft: '10px',
-							}}
-						>
-							<h2>{currentUser.firstName}</h2>
-							<Location user={currentUser} />
+			{({ data: { currentUser }, client }) => {
+				console.log(currentUser);
+				return (
+					<div className='Profile-Header'>
+						<div className='inner'>
+							<div
+								className='prof-img'
+								style={{ backgroundImage: `url(${currentUser.imageLarge})` }}
+							/>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									paddingLeft: '10px',
+								}}
+							>
+								<h2>
+									{currentUser.firstName} | {getAge(currentUser.dob)}
+								</h2>
+								<Location user={currentUser} />
+							</div>
 						</div>
 					</div>
-				</div>
-			)}
+				);
+			}}
 		</User>
 	);
 };
