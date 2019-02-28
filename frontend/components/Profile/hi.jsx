9c16ab1e6ea442d80bfabe69a0e4fd186c50fcab
Trevor Fehrman <trevorfehrman@gmail.com>
2019-02-28 00:25:19 -0800

    thing

--- frontend/components/Profile/hi.jsx
+++ frontend/components/Profile/hi.jsx
@@ -1,12 +1,7 @@
 const hi = () => {
-return (<div
-          className="prof-img"
-            style={{ backgroundImage: `url(${currentUser.imageLarge})` }}
-            />
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <h2>
-                    {currentUser.firstName} | {getAge(currentUser.dob)}
-                  </h2>
-                  <Location user={currentUser} />
-                </div>)
-}
+return (
+
+
+
+
+
