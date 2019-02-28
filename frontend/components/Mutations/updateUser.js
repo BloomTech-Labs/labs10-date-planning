b0273d6a4034c91d62b2062e867c687379486dc4
Lauren <llwor94@gmail.com>
2019-02-28 11:43:52 -0800

    like and unlike user working

--- frontend/components/Mutations/updateUser.js
+++ frontend/components/Mutations/updateUser.js
@@ -26,9 +26,48 @@ export const UPDATE_USER_MUTATION = gql`
 			biography
 			dob
 			gender
+			liked {
+				id
+			}
+			blocked {
+				id
+			}
 			genderPrefs
 			minAgePref
 			maxAgePref
+		}
+	}
+`;
+
+export const LIKE_USER_MUTATION = gql`
+	mutation LIKE_USER_MUTATION($like: ID) {
+		updateUser(data: { liked: { connect: { id: $like } } }) {
+			id
+			liked {
+				id
+			}
+		}
+	}
+`;
+
+export const UNLIKE_USER_MUTATION = gql`
+	mutation UNLIKE_USER_MUTATION($like: ID) {
+		updateUser(data: { liked: { disconnect: { id: $like } } }) {
+			id
+			liked {
+				id
+			}
+		}
+	}
+`;
+
+export const UPDATE_BLOCKS_MUTATION = gql`
+	mutation UPDATE_BLOCKS_MUTATION($block: ID) {
+		updateUser(data: { blocked: { connect: { id: $block } } }) {
+			id
+			blocked {
+				id
+			}
 		}
 	}
 `;
