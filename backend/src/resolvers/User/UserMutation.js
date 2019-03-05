ec431fd385c20a2934a78a7359674b113a1e65bd
ykrueng <yasirah.krueng@gmail.com>
2019-03-05 12:53:54 -0600

    add unblock and unlike user mutation

--- backend/src/resolvers/User/UserMutation.js
+++ backend/src/resolvers/User/UserMutation.js
@@ -5,6 +5,9 @@ module.exports = {
     // current user need to login
     if (!userId) throw new Error('You need to login to block a user')
 
+    // should not block yourself, that's suicidal
+    if ( userId === args.id) throw new Error('Please do not block yourself')
+
     // query user to block
     const blockedUser = await db.query.user({
       where: {
@@ -15,30 +18,54 @@ module.exports = {
     // check if user to block exist
     if (!blockedUser) throw new Error('User to block does not exist')
 
-    // update current user blocked list
-    return db.mutation.updateUser({
+    // query current user
+    const currentUser = await db.query.user({
       where: {
         id: userId
-      },
-      data: {
-        blocked: {
-          connect: {
-            id: args.id
-          }
+      }
+    }, `{ liked { id } }`)
+
+    if (currentUser.liked.findIndex(user => user.id === args.id) !== -1) {
+      return db.mutation.updateUser({
+        where: {
+          id: userId
         },
-        liked: {
-          disconnect: {
-            id: args.id
+        data: {
+          blocked: {
+            connect: {
+              id: args.id
+            }
+          },
+          liked: {
+            disconnect: {
+              id: args.id
+            }
           }
         }
-      }
-    }, info)
+      }, info)
+    } else {
+      return db.mutation.updateUser({
+        where: {
+          id: userId
+        },
+        data: {
+          blocked: {
+            connect: {
+              id: args.id
+            }
+          }
+        }
+      }, info)
+    }
   },
   async likeUser(parent, args, { request, db }, info) {
     const { userId } = request
 
     // current user need to login
-    if (!userId) throw new Error('You need to login to block a user')
+    if (!userId) throw new Error('You need to login to like a user')
+
+    // should not like yourself, that's creepy
+    if ( userId === args.id) throw new Error('Go find someone else to like, really!')
 
     // query user to like
     const userToLike = await db.query.user({
@@ -60,8 +87,8 @@ module.exports = {
       }
     }, `{blocked {id} }`)
     
-    // check if user to like is in current user block list
-    if (currentUser.blocked.findIndex(user => user.id === args.id) !== -1) throw new Error('User to like is in your blocked list')
+    // check if user to like is on current user block list
+    if (currentUser.blocked.findIndex(user => user.id === args.id) !== -1) throw new Error('User to like is on your blocked list')
 
     // update current user liked list
     return db.mutation.updateUser({
@@ -77,4 +104,63 @@ module.exports = {
       }
     }, info)
   },
+  async unblockUser(parent, args, { request, db }, info) {
+    const { userId } = request
+
+    // current user need to login
+    if (!userId) throw new Error('You need to login to unblock a user')
+
+    // query current user
+    const currentUser = await db.query.user({
+      where: {
+        id: userId
+      }
+    }, `{ blocked { id } }`)
+
+    // check if user to like is on current user liked list
+    if (currentUser.blocked.findIndex( user => user.id === args.id ) === -1) throw new Error('User is not on your blocked list')
+
+    // update current user liked list
+    return db.mutation.updateUser({
+      where: {
+        id: userId
+      },
+      data: {
+        blocked: {
+          disconnect: {
+            id: args.id
+          }
+        }
+      }
+    }, info)
+  },
+  async unlikeUser(parent, args, { request, db }, info) {
+    const { userId } = request
+
+    // current user need to login
+    if (!userId) throw new Error('You need to login to unblock a user')
+
+    // query current user
+    const currentUser = await db.query.user({
+      where: {
+        id: userId
+      }
+    }, `{ liked { id } }`)
+
+    if (currentUser.liked.findIndex( user => user.id === args.id ) === -1) throw new Error('User is not in your liked list')
+
+    // update current user liked list
+    return db.mutation.updateUser({
+      where: {
+        id: userId
+      },
+      data: {
+        liked: {
+          disconnect: {
+            id: args.id
+          }
+        }
+      }
+    }, info)
+  }
 };
