ced9ab7f52301e8b47c99c73de01a5c1702ecd78
Holden B <holden.bucher.92@gmail.com>
2019-03-10 14:26:48 -0500

    more tweaks

--- frontend/components/Queries/User.js
+++ frontend/components/Queries/User.js
@@ -51,12 +51,10 @@ export const isLoggedIn = async client => {
 	let response = await client.query({
 		query: CURRENT_USER_QUERY
 	});
-	// let data = client.cache.extract();
 	if (response) {
-		return { data: response.data };
+		return { currentUser: response.data };
 	}
 	return {};
-	// return Object.values(data).some(val => val.hasOwnProperty('currentUser'));
 };
 
 User.propTypes = {
