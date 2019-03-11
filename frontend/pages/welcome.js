2ffeeb3a6b8afbfa4cc622db6b2603027fa0498e
Holden B <holden.bucher.92@gmail.com>
2019-03-10 21:34:56 -0500

    fixed

--- frontend/pages/welcome.js
+++ frontend/pages/welcome.js
@@ -2,11 +2,9 @@ import Welcome from "../components/Welcome";
 import JoinUs from "./joinus";
 import User from "../components/Queries/User";
 
-const WelcomePage = props => (
+const WelcomePage = ({ query }) => (
 	<User>
 		{({ data, loading }) => {
-			const { query } = props;
-			console.log(props, "welcomepage props");
 			if (loading) return <div>loading</div>;
 			if (!data.currentUser) return <JoinUs />;
 			else return <Welcome user={data.currentUser} slug={query.slug} />;
