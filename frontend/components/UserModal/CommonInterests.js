9f1df8b8460d203a8f4825a00f611728baa8bff9
Lauren <llwor94@gmail.com>
2019-03-12 13:23:42 -0700

    okee

---
+++ frontend/components/UserModal/CommonInterests.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import NProgress from 'nprogress';
+import gql from 'graphql-tag';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Paper, Chip } from '@material-ui/core';
+import { MusicNote, LocalActivity, FitnessCenter } from '@material-ui/icons';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CardStyles from '../../static/jss/material-kit-pro-react/views/componentsSections/sectionCards';
+
+const CommonInterests = ({}) => {};
