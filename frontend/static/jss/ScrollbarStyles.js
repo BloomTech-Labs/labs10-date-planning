2cce747148799adcb48387cb77abc20ee755338d
Synch3 <jonsimo@aol.com>
2019-03-13 18:59:30 -0400

    scrollbar plus other stuff

--- frontend/static/jss/ScrollbarStyles.js
+++ frontend/static/jss/ScrollbarStyles.js
@@ -1,13 +1,16 @@
 const scrollbar = {
     '&::-webkit-scrollbar-track': {
     background: 'black',
+    outline: '1px solid #b2ddf7'
   },
   '&::-webkit-scrollbar': {
     width: 10,
     background: 'black',
+    outline: '1px solid #b2ddf7'
   },
   '&::-webkit-scrollbar-thumb': {
     background: 'black',
+    outline: '1px solid #b2ddf7'
   },
 }
 
