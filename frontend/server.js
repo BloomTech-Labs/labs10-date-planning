571fcc404e6bbea8aa1dabd4a83f82b6e8f6d386
Holden B <holden.bucher.92@gmail.com>
2019-03-15 00:34:10 -0500

    gimme all dem SSR routes

--- frontend/server.js
+++ frontend/server.js
@@ -1,68 +1,83 @@
 const express = require("express");
 const next = require("next");
 const port = parseInt(process.env.PORT, 10) || 3000;
-const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
-const handler = routes.getRequestHandler(app);
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-	express()
-		.use(handler)
-		.listen(port, err => {
-			if (err) throw err;
-			console.log(`Listening on http://localhost:${port}`);
-		});
+	const server = express();
+	server.get("/welcome/profile/:page/:subPage", (req, res) => {
+		const { page, subPage } = req.params;
+		const slug = subPage ? getSlug(page, subPage) : getSlug(page);
+		app.render(req, res, `/welcome`, { slug });
+	});
+	server.get("/welcome/profile/:page", (req, res) => {
+		const slug = getSlug(req.params.page);
+		app.render(req, res, `/welcome`, { slug });
+	});
+	server.get("/welcome/:page", (req, res) => {
+		const slug = getSlug(req.params.page);
+		app.render(req, res, `/welcome`, { slug });
+	});
+
+	server.get("/user/:userId", (req, res) => {
+		const { userId } = req.params;
+		app.render(req, res, `/home`, { user: userId });
+	});
+
+	server.get("/profile/user/:userId", (req, res) => {
+		const { userId } = req.params;
+		app.render(req, res, "/profile", { user: userId });
+	});
+
+	server.get("/profile/:category/user/:userId", (req, res) => {
+		const { category, userId } = req.params;
+		const slug = category === "chat" ? "chats" : category;
+		app.render(req, res, "/profile", { slug, user: userId });
+	});
+
+	server.get("/profile/:slug", (req, res) => {
+		const { slug } = req.params;
+		const slugObj = {
+			slug: slug === "chat" ? "chats" : slug
+		};
+		app.render(req, res, `/profile`, slugObj);
+	});
+
+	server.get("*", (req, res) => {
+		return handle(req, res);
+	});
+
+	server.listen(port, err => {
+		if (err) throw err;
+		console.log(`Listening on http://localhost:${port}`);
+	});
 });
 
-// app.prepare().then(() => {
-// 	const server = express();
-// 	server.get("/billing", (req, res) => app.render(req, res, "/billing"));
-// 	server.get("/joinus", (req, res) => app.render(req, res, "/joinus"));
-// 	server.get("/profile", (req, res) => {
-// 		app.render(req, res, "/profile");
-// 	});
-// 	server.get("/home", (req, res) => app.render(req, res, "/"));
-// 	server.get("/welcome", (req, res) => {
-// 		const actualPage = Object.assign({ slug: req.params.slug }, req.query);
-
-// 		app.render(req, res, "/welcome", actualPage);
-// 	});
-
-// 	server.get("/", (req, res) => app.render(req, res, "/"));
-
-// 	server.get("*", (req, res) => {
-// 		return handle(req, res);
-// 	});
-
-// 	server.listen(port, err => {
-// 		if (err) throw err;
-// 		console.log(`Listening on http://localhost:${port}`);
-// 	});
-// });
-
-// let actualPage;
-// const { slug } = req.params;
-// let actualPage;
-// switch (slug) {
-// 	case "0":
-// 		actualPage = "/profile/getstarted";
-// 	case "1":
-// 		actualPage = "/profile/gender";
-// 	case "2":
-// 		actualPage = "/profile/gender/preferences";
-// 	case "4":
-// 		actualPage = "/profile/age";
-// 	case "5":
-// 		actualPage = "/profile/age/preferences";
-// 	case "6":
-// 		actualPage = "/profile/location";
-// 	case "7":
-// 		actualPage = "/profile/images";
-// 	case "8":
-// 		actualPage = "/profile/about";
-// 	case "9":
-// 		actualPage = "/pro";
-// }
-// if (slug = "0") {
-//   path = '/welcome/profile/getstarted'
-// }
+const getSlug = (page, subPage = null) => {
+	if (subPage) {
+		page = `${page}/${subPage}`;
+	}
+	switch (page) {
+		case "getstarted":
+			return 0;
+		case "gender/preferences":
+			return 2;
+		case "gender":
+			return 1;
+		case "age/preferences":
+			return 4;
+		case "age":
+			return 3;
+		case "location":
+			return 5;
+		case "images":
+			return 6;
+		case "about":
+			return 7;
+		case "interests":
+			return 8;
+		case "goPro":
+			return 9;
+	}
+};
